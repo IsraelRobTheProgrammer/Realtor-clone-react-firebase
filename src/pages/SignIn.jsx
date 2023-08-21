8dafab64cb90cb0bfd928f2f11affbe40530d47e
Israel <israelrobinson965@gmail.com>
2023-08-21 21:17:03 +0100

    complete the sigin functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Oauth from "../components/oauth";
-
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
@@ -12,6 +13,24 @@ export default function SignIn() {
   });
 
   const { email, password } = formData;
+  const navigate = useNavigate()
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredentials.user){
+        navigate("/")
+      }
+    } catch (error) {
+      toast.error(error, "Something Ocurred");
+    }
+  };
 
   function changeState(e) {
     setFormData((prevState) => ({
@@ -34,7 +53,7 @@ export default function SignIn() {
         </div>
 
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               className="mb-6 w-full px-4 py-2 text-xl text-gray-800 bg-white border-gray-300 rounded-lg transition ease-in-out"
               type="email"
@@ -95,7 +114,7 @@ export default function SignIn() {
               <p className="text-center font-semibold"> OR </p>
             </div>
 
-            <Oauth/>
+            <Oauth />
           </form>
         </div>
       </div>
