d314230a1d0045cdd27ddf1ebcf5e52b8da5d3d2
Israel <israelrobinson965@gmail.com>
2023-08-25 22:10:44 +0100

    added a spinner component

--- src/components/privateroute.jsx
+++ src/components/privateroute.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router";
 import { useAuthStatus } from "../hooks/useAuthStatus";
-// import { toast } from "react-toastify";
+import Spinner from "./spinner";
 
 export default function PrivateRoute() {
   const { loggedIn, checkStatus } = useAuthStatus();
   console.log(loggedIn, "bbb", checkStatus);
 
   if (checkStatus) {
-    return <h3>Loading...</h3>;
+    return <Spinner />;
   }
+
   console.log("IN Private");
   return loggedIn ? <Outlet /> : <Navigate to={"/sign-in"} />;
 }
--- src/hooks/useAuthStatus.jsx
+++ src/hooks/useAuthStatus.jsx
@@ -1,10 +1,14 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useState, useEffect } from "react";
+// import { toast } from "react-toastify";
 
 export function useAuthStatus() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkStatus, setCheckStatus] = useState(true);
 
+  // if (!loggedIn) {
+  //   toast.error("You need to be signed in to continue")
+  // }
   useEffect(() => {
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
